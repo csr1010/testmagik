582487932be7d5eb8903b80837ce0c8a03b10adc
chetan jannu <chetan.jannu@oracle.com>
2020-11-24 12:18:23 -0500

    changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express  = require('express');  
 var http  = require('http'); 
 var app      = express();
-GLOBAL.mongojs = require('mongojs');
+global.mongojs = require('mongojs');
 //GLOBAL.db = mongojs('mongodb://csr:root@ds063307.mongolab.com:63307/xammagik',['userstests','examslisttests','questionstests','answertests' ]);
-GLOBAL.db = mongojs('mongodb://chetan10:chetan10@ds045099.mongolab.com:45099/testrun',['users','projects','accounts','runlist','JIRA','testcases']);
+global.db = mongojs('mongodb://chetan10:chetan10@ds045099.mongolab.com:45099/testrun',['users','projects','accounts','runlist','JIRA','testcases']);
 app.configure(function() {
 	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 	app.use(express.logger('dev')); 						// log every request to the console
@@ -19,11 +19,11 @@ var server = http.createServer(app).listen(process.env.PORT || process.env.OPENS
 console.log("App listening on port"+server.address().port);
 
 app.get('/#', function(req, res) {
-	res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+	res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
-app.get('/adminreg', function(req, res) {
-	res.sendfile('./public/adminreg.html'); 
+app.get('/adminreg', function (req, res) {
+	res.sendFile('./public/adminreg.html');
 });
 /*app.use(express.basicAuth(function(user, pass) {
 	return user === 'root' && pass === 'root';
